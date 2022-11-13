f5a8d3f3998f1ab578c6d6f55c12d76c700b0d05
Claire Morgenthau <claire_morgenthau@outlook.com>
2022-11-13 11:02:26 +0700

    Update state of licensing for GridSurvey

--- src/gridsurvey/scrape.py
+++ src/gridsurvey/scrape.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+# This source file uses data & API provided by Tyche Shepherd & gridsurvey.com
+
 import re
 import time
 from pathlib import Path
