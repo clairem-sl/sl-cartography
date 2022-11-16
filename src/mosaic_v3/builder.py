532b63cf779e275f0396d9708dc9065cff39e393
Pandu POLUAN <pepoluan@gmail.com>
2022-11-16 22:35:03 +0700

    Total rewrite of builder.py
    Now the logic used to build Nightlights map and Mosaic map are totally
    separated and no longer interspersed with one another.
    
    We extract them into their own separate classes. Plus the class for
    Mosaic map is genericized allowing more variations in the future if
    needed.

--- src/mosaic_v3/builder.py
+++ src/mosaic_v3/builder.py
@@ -2,6 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import time
+from abc import ABCMeta, abstractmethod
+from math import isqrt
 from pathlib import Path
 from typing import Dict, List, Set, Tuple
 
@@ -15,6 +17,199 @@ class OutOfBoundsError(ValueError):
     pass
 
 
+class WorldMapBuilder(metaclass=ABCMeta):
+    def __init__(
+        self,
+        regions: Dict[MapCoord, DominantColors],
+        seen_rows: Set[int],
+        corner1: MapCoord,
+        corner2: MapCoord,
+    ):
+        self.regions = regions
+        self.seen_rows = seen_rows
+        self.world_corner1 = corner1
+        self.world_corner2 = corner2
+        self._world_bounds = MapBounds.from_coords(corner1, corner2)
+
+    @property
+    def xmin(self):
+        return self._world_bounds.x_leftmost
+
+    @property
+    def xmax(self):
+        return self._world_bounds.x_rightmost
+
+    @property
+    def ymin(self):
+        return self._world_bounds.y_bottommost
+
+    @property
+    def ymax(self):
+        return self._world_bounds.y_topmost
+
+    @property
+    def width(self):
+        return self._world_bounds.width
+
+    @property
+    def height(self):
+        return self._world_bounds.height
+
+    def canvas_coord(self, tile_x: int, tile_y: int, multiplier: int = 1):
+        return (tile_x - self.xmin) * multiplier, (self.ymax - tile_y) * multiplier
+
+    @abstractmethod
+    def add_tile(self, coord: MapCoord, domc: DominantColors):
+        raise NotImplementedError
+
+
+class NightlightsMap(WorldMapBuilder):
+    NightlightsTileSize = 9
+    Black = 0
+    White = 255
+
+    def __init__(
+        self,
+        regions: Dict[MapCoord, DominantColors],
+        seen_rows: Set[int],
+        corner1: MapCoord,
+        corner2: MapCoord,
+    ):
+        super().__init__(regions, seen_rows, corner1, corner2)
+
+        canvas_box = MapCoord(self.width, self.height) * self.NightlightsTileSize
+        # Need noinspection here because "LA" is actually supported but PyCharm complains
+        # noinspection PyTypeChecker
+        self.canvas = Image.new("LA", canvas_box)
+
+        rect_row_black = Image.new(
+            "L", (self.width * self.NightlightsTileSize, self.NightlightsTileSize), color=self.Black
+        )
+        for y in self.seen_rows:
+            # x here MUST be 0, not x_min, because the coords here is relative to the canvas size, in pixels
+            self.canvas.paste(rect_row_black, (0, self.NightlightsTileSize * (self.ymax - y)))
+        self.sqw_3x3 = Image.new("L", (3, 3), color=self.White)
+
+    def world_has_all_of(self, *items: MapCoord) -> bool:
+        for i in items:
+            if not self.regions.get(i):
+                return False
+        return True
+
+    def world_has_none_of(self, *items: MapCoord) -> bool:
+        for i in items:
+            if self.regions.get(i):
+                return False
+        return True
+
+    def add_tile(self, coord: MapCoord, domc: DominantColors):
+        tile_sz = self.NightlightsTileSize
+        black = self.Black
+        white = self.White
+        sqw_3x3 = self.sqw_3x3
+
+        # region Compass points coordinates
+        c_n = coord + (0, 1)
+        c_e = coord + (1, 0)
+        c_w = coord - (1, 0)
+        c_s = coord - (0, 1)
+        c_ne = coord + (1, 1)
+        c_nw = coord + (-1, 1)
+        c_se = coord + (1, -1)
+        c_sw = coord + (-1, -1)
+        # endregion
+
+        tile_img = Image.new("L", (tile_sz, tile_sz), color=black)
+        tile_img.paste(sqw_3x3, (3, 3))
+        draw = ImageDraw.Draw(tile_img)
+
+        # region Vertical & Horizontal connections
+        if c_n in self.regions:
+            tile_img.paste(sqw_3x3, (3, 0))
+        if c_e in self.regions:
+            tile_img.paste(sqw_3x3, (6, 3))
+        if c_w in self.regions:
+            tile_img.paste(sqw_3x3, (0, 3))
+        if c_s in self.regions:
+            tile_img.paste(sqw_3x3, (3, 6))
+        # endregion
+
+        # region Diagonals
+        if self.world_has_all_of(c_n, c_e):
+            if c_ne in self.regions:
+                tile_img.paste(sqw_3x3, (6, 0))
+                if self.world_has_none_of(c_s, c_sw, c_w):
+                    draw.point((3, 5), fill=black)
+            else:
+                draw.point((6, 2), fill=white)
+        if self.world_has_all_of(c_n, c_w):
+            if c_nw in self.regions:
+                tile_img.paste(sqw_3x3, (0, 0))
+                if self.world_has_none_of(c_s, c_se, c_e):
+                    draw.point((5, 5), fill=black)
+            else:
+                draw.point((2, 2), fill=white)
+        if self.world_has_all_of(c_s, c_e):
+            if c_se in self.regions:
+                tile_img.paste(sqw_3x3, (6, 6))
+                if self.world_has_none_of(c_n, c_nw, c_w):
+                    draw.point((3, 3), fill=black)
+            else:
+                draw.point((6, 6), fill=white)
+        if self.world_has_all_of(c_s, c_w):
+            if c_sw in self.regions:
+                tile_img.paste(sqw_3x3, (0, 6))
+                if self.world_has_none_of(c_n, c_ne, c_e):
+                    draw.point((5, 3), fill=black)
+            else:
+                draw.point((2, 6), fill=white)
+        # endregion
+
+        self.canvas.paste(tile_img, self.canvas_coord(*coord, tile_sz))
+
+
+class MosaicMap(WorldMapBuilder):
+    MosaicSubtileSize = 2
+
+    def __init__(
+        self,
+        regions: Dict[MapCoord, DominantColors],
+        seen_rows: Set[int],
+        corner1: MapCoord,
+        corner2: MapCoord,
+        subtiles_domc_keys: List[str],
+    ):
+        super().__init__(regions, seen_rows, corner1, corner2)
+
+        self._domc_keys = subtiles_domc_keys
+        # See https://docs.python.org/3/library/math.html#math.isqrt
+        self._dim = 1 + isqrt(len(subtiles_domc_keys) - 1)
+
+        canvas_box = MapCoord(self.width, self.height) * self.MosaicSubtileSize * self._dim
+        self.canvas = Image.new("RGBA", canvas_box)
+
+    def paste_subtiles(self, target: Image.Image, size: int, subtile_colors: List[Tuple[int, int, int]]):
+        assert len(subtile_colors) == (size * size)
+        sx, sy = 0, 0
+        smax = size * self.MosaicSubtileSize
+        tile_boxsz = (self.MosaicSubtileSize, self.MosaicSubtileSize)
+        for color in subtile_colors:
+            loc = (sx, sy)
+            subtile = Image.new("RGBA", tile_boxsz, color=color)
+            target.paste(subtile, loc)
+            sx += self.MosaicSubtileSize
+            if sx >= smax:
+                sx = 0
+                sy += self.MosaicSubtileSize
+
+    def add_tile(self, coord: MapCoord, domc: DominantColors):
+        tile_sz = self.MosaicSubtileSize
+        tile_boxsz = MapCoord(tile_sz, tile_sz)
+        tile_mosaic = Image.new("RGBA", tile_boxsz * self._dim)
+        self.paste_subtiles(tile_mosaic, self._dim, domc.to_list(*self._domc_keys))
+        self.canvas.paste(tile_mosaic, self.canvas_coord(*coord, tile_sz * self._dim))
+
+
 def build_world_maps(
     regions: Dict[MapCoord, DominantColors],
     seen_rows: Set[int],
@@ -24,71 +219,15 @@ def build_world_maps(
     corner2: MapCoord,
 ):
     world_bounds = MapBounds.from_coords(corner1, corner2)
-    x_min, y_min, x_max, y_max = world_bounds
-    tot_width = x_max - x_min + 1
-    tot_height = y_max - y_min + 1
-
-    nl_tile_img_size = 9
-    mo_subtile_sz = 2
-    mo_subtile_boxsz = MapCoord(mo_subtile_sz, mo_subtile_sz)
-
-    Image.MAX_IMAGE_PIXELS = tot_width * nl_tile_img_size * tot_height * nl_tile_img_size
-
-    nl_black = 0
-    nl_white = 255
-
-    print("Building canvases")
     start_t = time.monotonic()
-    # Gotta disable Type Checker for this line, because the type hint says "LA" is not supported, while in fact, it is
-    # noinspection PyTypeChecker
-    canvas_nightlights = Image.new("LA", (tot_width * nl_tile_img_size, tot_height * nl_tile_img_size))
-    rect_row_black = Image.new("L", (tot_width * nl_tile_img_size, nl_tile_img_size), color=nl_black)
-    for y in seen_rows:
-        # x here MUST be 0, not x_min, because the coords here is relative to the canvas size, in pixels
-        canvas_nightlights.paste(rect_row_black, (0, nl_tile_img_size * (y_max - y)))
-    sqw_3x3 = Image.new("L", (3, 3), color=nl_white)
 
-    canvas_mosaic_1x1 = Image.new("RGBA", (tot_width * mo_subtile_sz, tot_height * mo_subtile_sz))
-    canvas_mosaic_2x2 = Image.new("RGBA", (tot_width * mo_subtile_sz * 2, tot_height * mo_subtile_sz * 2))
-    canvas_mosaic_3x3 = Image.new("RGBA", (tot_width * mo_subtile_sz * 3, tot_height * mo_subtile_sz * 3))
+    nightlights = NightlightsMap(regions, seen_rows, corner1, corner2)
+    mosaic_1x1 = MosaicMap(regions, seen_rows, corner1, corner2, ["full"])
+    mosaic_2x2 = MosaicMap(regions, seen_rows, corner1, corner2, ["q_nw", "q_ne", "q_sw", "q_se"])
+    mosaic_3x3 = MosaicMap(
+        regions, seen_rows, corner1, corner2, ["n_nw", "n_no", "n_ne", "n_we", "n_ce", "n_ea", "n_sw", "n_so", "n_se"]
+    )
 
-    # This is an 'unfold' of all(map(lambda x: x in regions_dict, items))
-    # Easier to read, imho
-    def world_has_all_of(*items):
-        for i in items:
-            assert isinstance(i, MapCoord)
-            if not regions.get(i):
-                return False
-        return True
-
-    def world_has_none_of(*items):
-        for i in items:
-            assert isinstance(i, MapCoord)
-            if regions.get(i):
-                return False
-        return True
-
-    def paste_subtiles(target: Image.Image, size: int, subtile_colors: List[Tuple[int, int, int]]):
-        assert len(subtile_colors) == (size * size)
-        sx, sy = 0, 0
-        smax = size * mo_subtile_sz
-        for color in subtile_colors:
-            loc = (sx, sy)
-            subtile = Image.new("RGBA", mo_subtile_boxsz, color=color)
-            target.paste(subtile, loc)
-            sx += mo_subtile_sz
-            if sx >= smax:
-                sx = 0
-                sy += mo_subtile_sz
-
-    # IMPORTANT NOTE:
-    # Even on a full-sized map with 2000x2000 tiles, this process takes less than 10 seconds
-    # So, please do NOT even consider of parallelizing this; it will be effort wasted for minimal
-    # performance improvement.
-    # Remember: premature optimization is the root of all evils
-    # Focus the optimizations elsewhere.
-
-    print(f"Processing world tiles... ", end="", flush=True)
     count = 0
     coord: MapCoord
     domc: DominantColors
@@ -98,77 +237,11 @@ def build_world_maps(
 
         if count % 100 == 0:
             print("|", end="", flush=True)
-            count = 0
 
-        c_n = coord + (0, 1)
-        c_e = coord + (1, 0)
-        c_w = coord - (1, 0)
-        c_s = coord - (0, 1)
-        c_ne = coord + (1, 1)
-        c_nw = coord + (-1, 1)
-        c_se = coord + (1, -1)
-        c_sw = coord + (-1, -1)
-
-        tile_img = Image.new("L", (nl_tile_img_size, nl_tile_img_size), color=nl_black)
-        tile_img.paste(sqw_3x3, (3, 3))
-        draw = ImageDraw.Draw(tile_img)
-
-        if c_n in regions:
-            tile_img.paste(sqw_3x3, (3, 0))
-        if c_e in regions:
-            tile_img.paste(sqw_3x3, (6, 3))
-        if c_w in regions:
-            tile_img.paste(sqw_3x3, (0, 3))
-        if c_s in regions:
-            tile_img.paste(sqw_3x3, (3, 6))
-
-        if world_has_all_of(c_n, c_e):
-            if c_ne in regions:
-                tile_img.paste(sqw_3x3, (6, 0))
-                if world_has_none_of(c_s, c_sw, c_w):
-                    draw.point((3, 5), fill=nl_black)
-            else:
-                draw.point((6, 2), fill=nl_white)
-        if world_has_all_of(c_n, c_w):
-            if c_nw in regions:
-                tile_img.paste(sqw_3x3, (0, 0))
-                if world_has_none_of(c_s, c_se, c_e):
-                    draw.point((5, 5), fill=nl_black)
-            else:
-                draw.point((2, 2), fill=nl_white)
-        if world_has_all_of(c_s, c_e):
-            if c_se in regions:
-                tile_img.paste(sqw_3x3, (6, 6))
-                if world_has_none_of(c_n, c_nw, c_w):
-                    draw.point((3, 3), fill=nl_black)
-            else:
-                draw.point((6, 6), fill=nl_white)
-        if world_has_all_of(c_s, c_w):
-            if c_sw in regions:
-                tile_img.paste(sqw_3x3, (0, 6))
-                if world_has_none_of(c_n, c_ne, c_e):
-                    draw.point((5, 3), fill=nl_black)
-            else:
-                draw.point((2, 6), fill=nl_white)
-
-        canvas_coord = MapCoord(coord.x - x_min, y_max - coord.y)
-
-        canvas_nightlights.paste(tile_img, tuple(canvas_coord * nl_tile_img_size))
-
-        tile_mosaic_1x1 = Image.new("RGBA", mo_subtile_boxsz, color=domc["full"])
-        canvas_mosaic_1x1.paste(tile_mosaic_1x1, tuple(canvas_coord * mo_subtile_sz))
-
-        tile_mosaic_2x2 = Image.new("RGBA", mo_subtile_boxsz * 2)
-        paste_subtiles(tile_mosaic_2x2, 2, domc.to_list("q_nw", "q_ne", "q_sw", "q_se"))
-        canvas_mosaic_2x2.paste(tile_mosaic_2x2, tuple(canvas_coord * (mo_subtile_sz * 2)))
-
-        tile_mosaic_3x3 = Image.new("RGBA", mo_subtile_boxsz * 3)
-        paste_subtiles(
-            tile_mosaic_3x3,
-            3,
-            domc.to_list("n_nw", "n_no", "n_ne", "n_we", "n_ce", "n_ea", "n_sw", "n_so", "n_se"),
-        )
-        canvas_mosaic_3x3.paste(tile_mosaic_3x3, tuple(canvas_coord * (mo_subtile_sz * 3)))
+        nightlights.add_tile(coord, domc)
+        mosaic_1x1.add_tile(coord, domc)
+        mosaic_2x2.add_tile(coord, domc)
+        mosaic_3x3.add_tile(coord, domc)
 
     elapsed_t = time.monotonic() - start_t
     print(f"{count} tiles processed in {elapsed_t:,.2f} seconds")
@@ -177,12 +250,12 @@ def build_world_maps(
     start_t = time.monotonic()
 
     nightlights_path.parent.mkdir(parents=True, exist_ok=True)
-    canvas_nightlights.save(nightlights_path, optimize=True)
+    nightlights.canvas.save(nightlights_path, optimize=True)
 
     mosaic_path.parent.mkdir(parents=True, exist_ok=True)
-    canvas_mosaic_1x1.save(mosaic_path.with_suffix(".1x1.png"), optimize=True)
-    canvas_mosaic_2x2.save(mosaic_path.with_suffix(".2x2.png"), optimize=True)
-    canvas_mosaic_3x3.save(mosaic_path.with_suffix(".3x3.png"), optimize=True)
+    mosaic_1x1.canvas.save(mosaic_path.with_suffix(".1x1.png"), optimize=True)
+    mosaic_2x2.canvas.save(mosaic_path.with_suffix(".2x2.png"), optimize=True)
+    mosaic_3x3.canvas.save(mosaic_path.with_suffix(".3x3.png"), optimize=True)
 
     elapsed_t = time.monotonic() - start_t
     print(f"{elapsed_t:,.2f} seconds")
