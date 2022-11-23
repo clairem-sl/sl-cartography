5c6f2b4fba0cffe4af19cc01a051a4fb8ff5c69c
Claire Morgenthau <claire_morgenthau@outlook.com>
2022-11-23 10:04:15 +0700

    When drawing black, extend the end lines.

--- src/cartographer/roadmapper/road.py
+++ src/cartographer/roadmapper/road.py
@@ -25,6 +25,31 @@ class Point(NamedTuple):
     y: int
 
 
+def extend_by_n(p1: Point, p2: Point, n: int):
+    """Extend by at least n pixels."""
+    x1, y1 = p1
+    x2, y2 = p2
+    dx = x2 - x1
+    dy = y2 - y1
+    if abs(dx) > abs(dy):
+        # Horizontally oriented
+        _n = n if dx >= 0 else -n
+        x3 = x2 + _n
+        y3 = (x3 - x1) * dy / dx + y1
+    else:
+        # Vertically oriented
+        _n = n if dy >= 0 else -n
+        y3 = y2 + _n
+        x3 = (y3 - y1) * dx / dy + x1
+    return Point(round(x3), round(y3))
+
+
+# pp1 = Point(1, 1)
+# pp2 = Point(2, 3)
+# print(extend_by_n(pp1, pp2, 2))
+# print(extend_by_n(pp2, pp1, 2))
+
+
 class Segment:
     BlackWidth = 35
     ColorWidth = 25
@@ -40,6 +65,24 @@ class Segment:
     def add(self, point: Point):
         self.points.append(point)
 
+    @staticmethod
+    def _draw_line(draw, points, width, fill, extend_by: int = 0):
+        seen = set()
+        uniques = []
+        for p in points:
+            if p in seen:
+                continue
+            seen.add(p)
+            uniques.append(p)
+        if len(uniques) < 2:
+            return
+        points = uniques
+        if extend_by:
+            new_p0 = extend_by_n(points[1], points[0], extend_by)
+            new_pz = extend_by_n(points[-2], points[-1], extend_by)
+            points = [new_p0, *points[1:-1], new_pz]
+        draw.line(points, width=width, fill=fill, joint="curve")
+
     def _draw_dashed(
         self,
         draw: ImageDraw.ImageDraw,
@@ -48,6 +91,7 @@ class Segment:
         start_blank: bool = True,
         dash_len: int = 6,
         blank_len: int = 4,
+        extend_by: int = 0,
     ):
         c = -1 if (blank := start_blank) else 0
         piece_points: list[Point] = []
@@ -62,23 +106,23 @@ class Segment:
                 continue
             piece_points.append(point)
             if c >= dash_len:
-                draw.line(piece_points, width=width, fill=color, joint="curve")
+                self._draw_line(draw, piece_points, width=width, fill=color, extend_by=extend_by)
                 c = 0
                 blank = True
                 piece_points.clear()
         if not blank and piece_points:
-            draw.line(piece_points, width=width, fill=color, joint="curve")
+            self._draw_line(draw, piece_points, width=width, fill=color, extend_by=extend_by)
 
-    def _draw_solid(self, draw: ImageDraw.ImageDraw, width: int, color: tuple[int, int, int]):
-        draw.line(self.points, width=width, fill=color, joint="curve")
+    def _draw_solid(self, draw: ImageDraw.ImageDraw, width: int, color: tuple[int, int, int], extend_by: int = 0):
+        self._draw_line(draw, self.points, width=width, fill=color, extend_by=extend_by)
 
     def draw_black(self, draw: ImageDraw.ImageDraw):
         if not self.points:
             return
         if self.mode == DrawMode.SOLID:
-            self._draw_solid(draw, self.BlackWidth, (0, 0, 0))
+            self._draw_solid(draw, self.BlackWidth, (0, 0, 0), extend_by=3)
         elif self.mode == DrawMode.DASHED:
-            self._draw_dashed(draw, self.BlackWidth, (0, 0, 0))
+            self._draw_dashed(draw, self.BlackWidth, (0, 0, 0), extend_by=3)
         else:
             raise NotImplementedError()
 
