18e33997fc9ea82a3142c0b1dea973dd9f8699d4
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-04-21 09:38:21 +0700

    QnD exporter to YAML

---
+++ src/region_auditor/exportDB.py
@@ -0,0 +1,59 @@
+import pickle
+
+import ruamel.yaml as ryaml
+
+from datetime import datetime
+from pathlib import Path
+from pprint import pprint
+from typing import Any
+
+
+DB_PATH = Path(r"C:\Cache\SL-Carto\RegionsDB.pkl")
+
+def main():
+    result: dict[str, dict[str, Any]] = {}
+    with DB_PATH.open("rb") as fin:
+        data: dict[tuple[int, int], dict[str, Any]] = pickle.load(fin)
+    print(f"Retrieved {len(data)} records. Transforming...", end="", flush=True)
+    for coord, info in data.items():
+        x, y = coord
+        info["sources"] = list(info["sources"])
+        result[f"{x},{y}"] = info
+
+    print("\nRecords transformed. Exporting...", end="", flush=True)
+    exported = {
+        "_schema": {
+            "name": "sl-carto-regionsdb",
+            "version": "1.0.0",
+            "desc": {
+                "current_name": "Current name of region as of time of audit",
+                "first_seen": "Timestamp of audit when region was first detected (as non-void)",
+                "last_check": "Timestamp of last audit when region was checked",
+                "last_seen": "Timestamp of audit when region was last seen (as non-void)",
+                "name_history": (
+                    "A dict of name:[timestamps], where each entry in the timestamps list is timestamp of "
+                    "last audit when region was detected using that name. This enables proper chronology in the "
+                    "rare but possible situation of a region going Name1->Name2->Name1."
+                ),
+                "sources": (
+                    "Sources of information used to generate the record. 'cap' is SL's cap server. "
+                    "'bb' is BonnieBots database."
+                )
+            }
+        },
+        "_metadata": {
+            "created": datetime.now().astimezone().isoformat(timespec="minutes")
+        },
+        "data": result
+    }
+    yml_path = DB_PATH.with_suffix(".yaml")
+    with yml_path.open("wt") as fout:
+        ryaml.dump(exported, fout)
+
+    print(f"\nExported to {yml_path}")
+    # with yml_path.open("rt") as fin:
+    #     print(fin.read())
+
+
+if __name__ == '__main__':
+    main()
