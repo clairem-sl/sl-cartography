ba7b7eb39fd0350255e977e20daba21ca2969c3c
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-05-02 23:46:17 +0700

    WorldMap.Mosaic

--- src/worldmap_v4/mosaic.py
+++ src/worldmap_v4/mosaic.py
@@ -3,19 +3,21 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import argparse
 import pickle
+import re
 from itertools import product
-from math import isqrt
 
 import httpx
 import ruamel.yaml as ryaml
 
 from pathlib import Path
-from typing import Any, Final, Iterable, Protocol, TypedDict, cast, Sized, Sequence
+from typing import Any, Final, Protocol, TypedDict, cast
 
-from PIL import Image, ImageDraw
+from PIL import Image
 
-from sl_maptools import MapCoord, MapRegion
-from worldmap_v4.color_processing import DominantColors
+from sl_maptools import MapCoord
+
+
+RE_MAPFILE = re.compile(r"^(\d+)-(\d+)_\d")
 
 
 BONNIE_REGDB_URL = "https://www.bonniebots.com/static-api/regions/index.json"
@@ -61,14 +63,14 @@ def tuptup(*items):
     yield from product(items, items)
 
 
-SLABS: dict[int, list[tuple[int, int, int, int]]] = {
+FASCIA_COORDS: dict[int, list[tuple[int, int, int, int]]] = {
     1: [(0, 0, 256, 256)],
     2: [getbox(2, 1, x, y) for x, y in tuptup(0, 1)],
     3: [getbox(16, 6, x, y) for x, y in tuptup(0, 5, 10)],
     4: [getbox(4, 1, x, y) for x, y in tuptup(0, 1, 2, 3)],
     5: [getbox(128, 28, x, y) for x, y in tuptup(0, 25, 50, 75, 100)],
 }
-SLAB_SIZES: dict[int, int] = {
+FASCIA_SIZES: dict[int, int] = {
     1: 3,
     2: 3,
     3: 3,
@@ -84,7 +86,6 @@ class Options(Protocol):
     force: bool
     mosaicfile: Path
     mapdir: Path
-    slabsize: int
 
 
 class BonnieRegionData(TypedDict):
@@ -94,17 +95,14 @@ class BonnieRegionData(TypedDict):
 
 
 def get_opts():
-    parser = argparse.ArgumentParser("worldmap_v4.nightlights")
+    parser = argparse.ArgumentParser("worldmap_v4.mosaic")
 
     parser.add_argument("--dbpath", type=Path, default=DEFA_DB_PATH)
     parser.add_argument("--mapdir", type=Path, default=DEFA_MAPS_DIR)
-    parser.add_argument("--slabsize", type=int, default=DEFA_MOSAIC_SLAB_SZ)
 
     grp = parser.add_mutually_exclusive_group()
     grp.add_argument("--bonniedb", type=Path, default=None)
     grp.add_argument("--fetchbonnie", action="store_true")
-
-    parser.add_argument("mosaicfile", type=Path)
 
     parser.add_argument("--force", action="store_true")
 
@@ -113,151 +111,62 @@ def get_opts():
     return _opts
 
 
-def canvas_coord(region_x: int, region_y: int, multiplier: int = 1) -> tuple[int, int]:
-    """
-    Converts geo-coords (in units of Regions) to canvas coords (in units of pixels)
+MapFiles: dict[tuple[int, int], Path] = {}
 
-    This is not a simple multiplied-items tuple like what's implemented by MapCoord.__mul__
-
-    This method implements the shift, reflect, and multiplication necessary to do the conversion.
-
-    :param region_x: X geo-coordinate of the Region
-    :param region_y: Y geo-coordinate of the Region
-    :param multiplier: Optional multiplier
-    :return: The canvas coordinate for the Region
-    """
-    return (region_x - MIN_X) * multiplier, (MAX_Y - region_y) * multiplier
-
-
-def make_mosaic_one(domc_all: dict[MapCoord, DominantColors], mosaic_keys: Sequence[str], *, slab_size: int = 2):
-    _dim = 1 + isqrt(len(mosaic_keys) - 1)
+def make_one_mosaic(mo_pattern: int, regions: set[MapCoord], mapdir: Path):
+    fascia_size = FASCIA_SIZES[mo_pattern]
+    slab_size = fascia_size * mo_pattern
     width = MAX_X - MIN_X + 1
     height = MAX_Y - MIN_Y + 1
-    canvas_size = MapCoord(width, height) * slab_size * _dim
-    canvas = Image.new("RGBA", canvas_size)
-    slab_box = MapCoord(slab_size, slab_size)
-    mosaic_box = slab_box * _dim
-    smax = _dim * slab_size
-
-    for coord, domc in domc_all.items():
-        mosaic_tile = Image.new("RGBA", mosaic_box)
-        sx = sy = 0
-        for colr in domc.to_list(*mosaic_keys):
-            slab = Image.new("RGB", slab_box, color=colr)
-            mosaic_tile.paste(slab, (sx, sy))
-            sx += slab_size
-            if sx >= smax:
-                sx = 0
-                sy += slab_size
-        canvas.paste(mosaic_tile, canvas_coord(*coord, mosaic_box.x))
-
-
-def make_mosaic(mapdir: Path, regions: set[MapCoord], *, slab_size: int) -> Image.Image:
-    width = MAX_X - MIN_X + 1
-    height = MAX_Y - MIN_Y + 1
-
-    for mo in range(1, 6):
-
-
     canvas_size = MapCoord(width, height) * slab_size
+    canvas = Image.new("RGBA", canvas_size)
+    fascia_box = MapCoord(fascia_size, fascia_size)
+    # slab_box = MapCoord(slab_size, slab_size)
 
-    def box(value: int) -> tuple[int, int]:
-        return value, value
+    targ = mapdir / f"worldmap_mosaic_v4_{mo_pattern}x{mo_pattern}.png"
+    print(f"\nSaving as {targ}")
+    print(f"Creating {mo_pattern}x{mo_pattern} ", end="", flush=True)
+    for i, coord in enumerate(regions, start=1):
+        if i % 100 == 0:
+            canvas.save(targ)
+            print(".", end="", flush=True)
+        if coord not in MapFiles:
+            continue
+        mapfile = MapFiles[coord]
+        # print(mapfile, end=" ")
+        with mapfile.open("rb") as fin:
+            regimg = Image.open(fin)
+            regimg.load()
+        fx = (coord.x - MIN_X) * fascia_size
+        fy = (MAX_Y - coord.y) * fascia_size
+        sx = sy = 0
+        for fascia in FASCIA_COORDS[mo_pattern]:
+            # print(fascia, end=" ")
+            fascia_img = regimg.crop(fascia)
+            quant = fascia_img.quantize(colors=16, kmeans=3)
+            rgb = quant.convert("RGB")
+            colors = cast(list[tuple[int, tuple[int, int, int]]], rgb.getcolors())
+            freq, dom = max(colors, key=lambda x: x[0])
+            # print(dom)
+            loc = fx + sx, fy + sy
+            canvas.paste(Image.new("RGB", fascia_box, color=dom), loc)
+            sy += fascia_size
+            if sy >= slab_size:
+                sy = 0
+                sx += fascia_size
 
-    canvas_box = MapCoord(width, height) * region_size
-    canvas = Image.new("L", canvas_box, color=BLACK)
-    slab_w = Image.new("L", box(slab_sz), color=WHITE)
+    canvas.save(targ)
+    print(f"Saved to {targ}")
 
-    def world_has_all_of(*coords: MapCoord) -> bool:
-        return all(map(regions.__contains__, coords))
 
-    def world_has_none_of(*coords: MapCoord) -> bool:
-        return not any(map(regions.__contains__, coords))
-
-    for coord in regions:
-        # region Compass points coordinates
-        c_n = coord + (0, 1)
-        c_e = coord + (1, 0)
-        c_w = coord - (1, 0)
-        c_s = coord - (0, 1)
-        c_ne = coord + (1, 1)
-        c_nw = coord + (-1, 1)
-        c_se = coord + (1, -1)
-        c_sw = coord + (-1, -1)
-        # endregion
-
-        region_img = Image.new("L", box(region_size), color=BLACK)
-        region_img.paste(slab_w, box(slab_sz))
-        draw = ImageDraw.Draw(region_img)
-
-        # region Vertical & Horizontal connections
-        if world_has_all_of(c_n):
-            region_img.paste(slab_w, (slab_sz, 0))
-        if world_has_all_of(c_e):
-            region_img.paste(slab_w, (slab_sz * 2, slab_sz))
-        if world_has_all_of(c_w):
-            region_img.paste(slab_w, (0, slab_sz))
-        if world_has_all_of(c_s):
-            region_img.paste(slab_w, (slab_sz, slab_sz * 2))
-        # endregion
-
-        # region Diagonals
-
-        # Far all these:
-        # if has_vert_neighbor and has_horiz_neighbor:
-        #   if also has_diag_neighbor_between_vert_and_horiz:
-        #     plop a slab
-        #   else:
-        #     chamfer_inner_corner
-        #   -plus-
-        #   if no_neighbor_on_other_sides:
-        #     chamfer_outer_corner
-
-        if world_has_all_of(c_n, c_e):
-            if world_has_all_of(c_ne):
-                region_img.paste(slab_w, (slab_sz * 2, 0))
-            else:
-                draw.point((slab_sz * 2, slab_sz - 1), fill=WHITE)
-            if world_has_none_of(c_s, c_sw, c_w):
-                draw.point((slab_sz, slab_sz * 2 - 1), fill=BLACK)
-
-        if world_has_all_of(c_n, c_w):
-            if world_has_all_of(c_nw):
-                region_img.paste(slab_w, (0, 0))
-            else:
-                draw.point(box(slab_sz - 1), fill=WHITE)
-            if world_has_none_of(c_s, c_se, c_e):
-                draw.point(box(slab_sz * 2 - 1), fill=BLACK)
-
-        if world_has_all_of(c_s, c_e):
-            if world_has_all_of(c_se):
-                region_img.paste(slab_w, box(slab_sz * 2))
-            else:
-                draw.point(box(slab_sz * 2), fill=WHITE)
-            if world_has_none_of(c_n, c_nw, c_w):
-                draw.point((slab_sz, slab_sz), fill=BLACK)
-
-        if world_has_all_of(c_s, c_w):
-            if world_has_all_of(c_sw):
-                region_img.paste(slab_w, (0, slab_sz * 2))
-            else:
-                draw.point((slab_sz - 1, slab_sz * 2), fill=WHITE)
-            if world_has_none_of(c_n, c_ne, c_e):
-                draw.point((slab_sz * 2 - 1, slab_sz), fill=BLACK)
-
-        # endregion
-
-        canvas.paste(region_img, canvas_coord(*coord, region_size))
-
-    return canvas
+def make_mosaic(mapdir: Path, regions: set[MapCoord]):
+    for mo in FASCIA_COORDS:
+        make_one_mosaic(mo, regions, mapdir)
 
 
 def make_map(opts: Options):
     dbpath = opts.dbpath
     bonniedb = opts.bonniedb
-
-    if not opts.force and opts.mosaicfile.exists():
-        raise FileExistsError(f"{opts.mosaicfile} already exists but --force not specified!")
 
     print(f"Reading Auditor's DB from {dbpath} ... ", end="", flush=True)
     with dbpath.open("rb") as fin:
@@ -280,12 +189,20 @@ def make_map(opts: Options):
         regions = regions.intersection(bonnie_coords)
         print(flush=True)
 
-    print("Creating Nightlights Map ... ", end="", flush=True)
-    canvas = make_mosaic(opts.mapdir, {MapCoord(x, y) for x, y in regions}, slab_size=opts.slabsize)
-    print("\nSaving nightlights mosaic ... ", end="", flush=True)
-    opts.mosaicfile.parent.mkdir(parents=True, exist_ok=True)
-    canvas.save(opts.mosaicfile, optimize=True)
-    print(f"\nNightlights mosaic saved to {opts.mosaicfile}")
+    print("Inventorizing region maps ... ", end="", flush=True)
+    all_maps = sorted(opts.mapdir.glob("*.jpg"))
+    for mapfile in all_maps:
+        m = RE_MAPFILE.match(mapfile.name)
+        x = int(m.group(1))
+        y = int(m.group(2))
+        MapFiles[x, y] = mapfile
+
+    print("\nCreating Mosaic Maps ... ", end="", flush=True)
+    make_mosaic(opts.mapdir, {MapCoord(x, y) for x, y in regions})
+    # print("\nSaving nightlights mosaic ... ", end="", flush=True)
+    # opts.mosaicfile.parent.mkdir(parents=True, exist_ok=True)
+    # canvas.save(opts.mosaicfile, optimize=True)
+    # print(f"\nNightlights mosaic saved to {opts.mosaicfile}")
 
 
 if __name__ == '__main__':
