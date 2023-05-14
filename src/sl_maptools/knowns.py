178a20201b60dabfff24ac527d22c228cf015f5d
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-05-14 22:11:35 +0700

    Manually merge knowns.py

--- src/sl_maptools/knowns.py
+++ src/sl_maptools/knowns.py
@@ -44,9 +44,54 @@ KNOWN_AREAS: Final[dict[str, AreaBounds]] = {
     "SeabirdIslands": AreaBounds(686, 920, 692, 928),
     "TropicalCoralIslands": AreaBounds(479, 1271, 485, 1280),
     "Gooseberry-Briarwood": AreaBounds(666, 1423, 674, 1429),
+    "TrinidadTobago": AreaBounds(x_westmost=902, y_southmost=1372, x_eastmost=906, y_northmost=1376),
+    "NativeIslands": AreaBounds(x_westmost=733, y_southmost=1367, x_eastmost=738, y_northmost=1371),
+    "MauiIslandEstates": AreaBounds(x_westmost=548, y_southmost=1137, x_eastmost=551, y_northmost=1140),
+    "Rosehaven": AreaBounds(x_westmost=906, y_southmost=1029, x_eastmost=909, y_northmost=1034),
+    "TortugaIslands": AreaBounds(x_westmost=500, y_southmost=1010, x_eastmost=504, y_northmost=1013),
+    "SeductionEstate": AreaBounds(x_westmost=757, y_southmost=926, x_eastmost=763, y_northmost=932),
+    "AntiquityLand": AreaBounds(x_westmost=763, y_southmost=909, x_eastmost=766, y_northmost=913),
+    "NewHaven": AreaBounds(x_westmost=597, y_southmost=1181, x_eastmost=603, y_northmost=1184),
+    "TheGroveEstate": AreaBounds(x_westmost=1000, y_southmost=1166, x_eastmost=1008, y_northmost=1171),
+    "TheWastelands": AreaBounds(x_westmost=785, y_southmost=1036, x_eastmost=787, y_northmost=1039),
+    "CedarCreek": AreaBounds(x_westmost=849, y_southmost=1342, x_eastmost=853, y_northmost=1346),
+    "PlumIslandSoundEstate": AreaBounds(x_westmost=921, y_southmost=1168, x_eastmost=923, y_northmost=1171),
+    "AMEstate": AreaBounds(x_westmost=606, y_southmost=1164, x_eastmost=608, y_northmost=1167),
+    "CalasGaladhonPark": AreaBounds(x_westmost=709, y_southmost=1110, x_eastmost=713, y_northmost=1113),
+    "IsleOfWyrms": AreaBounds(x_westmost=545, y_southmost=1059, x_eastmost=548, y_northmost=1063),
+    "AmazonRiver": AreaBounds(x_westmost=832, y_southmost=969, x_eastmost=835, y_northmost=972),
+    "Baunatal": AreaBounds(x_westmost=715, y_southmost=922, x_eastmost=718, y_northmost=924),
     # endregion
+    # region ### User Continents - Tentative
+    "GoreanLands": AreaBounds(x_westmost=1014, y_southmost=1175, x_eastmost=1020, y_northmost=1179),
+    "AngelManor": AreaBounds(x_westmost=1054, y_southmost=1397, x_eastmost=1056, y_northmost=1400),
+    "NakedEstates": AreaBounds(x_westmost=1038, y_southmost=1161, x_eastmost=1042, y_northmost=1163),
+    "Western": AreaBounds(x_westmost=963, y_southmost=1145, x_eastmost=967, y_northmost=1149),
+    "Babbage": AreaBounds(x_westmost=630, y_southmost=1004, x_eastmost=633, y_northmost=1007),
+    "Capitol": AreaBounds(x_westmost=641, y_southmost=995, x_eastmost=645, y_northmost=998),
+    "SunIslands": AreaBounds(x_westmost=876, y_southmost=987, x_eastmost=878, y_northmost=990),
+    "Mieville": AreaBounds(x_westmost=533, y_southmost=1196, x_eastmost=537, y_northmost=1201),
+    "Pirates1700": AreaBounds(x_westmost=940, y_southmost=1174, x_eastmost=942, y_northmost=1177),
+    "Mythera": AreaBounds(x_westmost=720, y_southmost=1324, x_eastmost=722, y_northmost=1328),
+    "SecondFrance": AreaBounds(x_westmost=556, y_southmost=1117, x_eastmost=559, y_northmost=1120),
+    # endregion
+    # region ### Premium Continents
+    "EastPremium": AreaBounds(x_westmost=1192, y_southmost=907, x_eastmost=1215, y_northmost=926),
+    "SouthPremium-North": AreaBounds(x_westmost=1013, y_southmost=811, x_eastmost=1028, y_northmost=828),
+    "SouthPremium-Middle": AreaBounds(x_westmost=1000, y_southmost=750, x_eastmost=1022, y_northmost=768),
+    "SouthPremium-South": AreaBounds(x_westmost=1000, y_southmost=500, x_eastmost=1022, y_northmost=518),
+    # endregion
+    # region ### Special Areas
+    "LindenLabDE": AreaBounds(x_westmost=1105, y_southmost=1382, x_eastmost=1108, y_northmost=1388),
     "LindenEstateServices": AreaBounds(1025, 1014, 1031, 1016),
     "MoleIslands": AreaBounds(1006, 971, 1011, 976),
+    "SLBRegions": AreaBounds(x_westmost=390, y_southmost=367, x_eastmost=395, y_northmost=372),
+    "SSP-15xx": AreaBounds(x_westmost=1155, y_southmost=1379, x_eastmost=1165, y_northmost=1383),
+    "SSP-40xx": AreaBounds(x_westmost=1182, y_southmost=1371, x_eastmost=1187, y_northmost=1377),
+    "LR-160": AreaBounds(x_westmost=1187, y_southmost=1206, x_eastmost=1190, y_northmost=1210),
+    "LR-180": AreaBounds(x_westmost=1193, y_southmost=1206, x_eastmost=1196, y_northmost=1210),
+    "Preflight": AreaBounds(x_westmost=1296, y_southmost=1193, x_eastmost=1299, y_northmost=1196),
+    # endregion
 }
 
 VERIFIED_VOIDS: Final[dict["MapCoord", datetime.date]] = {
