1772176dd18e977f0487edd0dd8e68b20e06cbaf
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-05-14 20:30:23 +0700

    More newly-identified interesting areas (Threshold >= 10)

--- src/sl_maptools/knowns.py
+++ src/sl_maptools/knowns.py
@@ -53,7 +53,18 @@ KNOWN_AREAS: Final[dict[str, AreaBounds]] = {
     "AntiquityLand": AreaBounds(x_westmost=763, y_southmost=909, x_eastmost=766, y_northmost=913),
     "NewHaven": AreaBounds(x_westmost=597, y_southmost=1181, x_eastmost=603, y_northmost=1184),
     "TheGroveEstate": AreaBounds(x_westmost=1000, y_southmost=1166, x_eastmost=1008, y_northmost=1171),
+    # region ## Tentative Naming
     "GoreanLands": AreaBounds(x_westmost=1014, y_southmost=1175, x_eastmost=1020, y_northmost=1179),
+    "AngelManor": AreaBounds(x_westmost=1054, y_southmost=1397, x_eastmost=1056, y_northmost=1400),
+    "NakedEstates": AreaBounds(x_westmost=1038, y_southmost=1161, x_eastmost=1042, y_northmost=1163),
+    "Western": AreaBounds(x_westmost=963, y_southmost=1145, x_eastmost=967, y_northmost=1149),
+    "TheWastelands": AreaBounds(x_westmost=785, y_southmost=1036, x_eastmost=787, y_northmost=1039),
+    "Babbage": AreaBounds(x_westmost=630, y_southmost=1004, x_eastmost=633, y_northmost=1007),
+    "Capitol": AreaBounds(x_westmost=641, y_southmost=995, x_eastmost=645, y_northmost=998),
+    "SunIslands": AreaBounds(x_westmost=876, y_southmost=987, x_eastmost=878, y_northmost=990),
+    "Mieville": AreaBounds(x_westmost=533, y_southmost=1196, x_eastmost=537, y_northmost=1201),
+    "Pirates1700": AreaBounds(x_westmost=940, y_southmost=1174, x_eastmost=942, y_northmost=1177),
+    # endregion
     # endregion
     # region ### Premium Continents
     "EastPremium": AreaBounds(x_westmost=1192, y_southmost=907, x_eastmost=1215, y_northmost=926),
@@ -67,6 +78,9 @@ KNOWN_AREAS: Final[dict[str, AreaBounds]] = {
     "SLBRegions": AreaBounds(x_westmost=390, y_southmost=367, x_eastmost=395, y_northmost=372),
     "SSP-15xx": AreaBounds(x_westmost=1155, y_southmost=1379, x_eastmost=1165, y_northmost=1383),
     "SSP-40xx": AreaBounds(x_westmost=1182, y_southmost=1371, x_eastmost=1187, y_northmost=1377),
+    "LR-160": AreaBounds(x_westmost=1187, y_southmost=1206, x_eastmost=1190, y_northmost=1210),
+    "LR-180": AreaBounds(x_westmost=1193, y_southmost=1206, x_eastmost=1196, y_northmost=1210),
+    "Preflight": AreaBounds(x_westmost=1296, y_southmost=1193, x_eastmost=1299, y_northmost=1196),
     # endregion
 }
 
