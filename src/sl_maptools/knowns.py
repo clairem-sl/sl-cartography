75b6fef922fc10ca7bad710c684132f5126a59d2
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-05-20 19:07:13 +0700

    More #

--- src/sl_maptools/knowns.py
+++ src/sl_maptools/knowns.py
@@ -108,7 +108,7 @@ KNOWN_AREAS: Final[dict[str, AreaBounds]] = {
     "LR-160": AreaBounds(1187, 1206, 1190, 1210),
     "LR-180": AreaBounds(1193, 1206, 1196, 1210),
     "Preflight": AreaBounds(1296, 1193, 1299, 1196),
-    "TheMists": AreaBounds(562, 734, 567, 739),  ## Former site of Fantasy Faire 2023
+    "TheMists": AreaBounds(562, 734, 567, 739),  ### Former site of Fantasy Faire 2023
     "PaleoQuest": AreaBounds(400, 392, 401, 400),
     # endregion
 }
