5555712a960dd56289b3b1670769abeefd76bd15
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-05-28 16:00:56 +0700

    Extract recent logic to its own function

--- src/analysis/recent.py
+++ src/analysis/recent.py
@@ -3,38 +3,45 @@ import pickle
 from datetime import datetime, timedelta
 from pathlib import Path
 from pprint import pprint
+from typing import NamedTuple
 
 from sl_maptools import CoordType, RegionsDBRecord3, AreaBounds
 
 DB_PATH = Path(r"C:\Cache\SL-Carto\RegionsDB3.pkl")
 CUTOFF = 3
 
+_NAO = datetime.now().astimezone()
+
+DATABASE: dict[CoordType, RegionsDBRecord3]
+
+
+class InterestingRegion(NamedTuple):
+    timestamp: datetime
+    name: str
+    coord: CoordType
+
+
+def recent(max_days: int) -> set[InterestingRegion]:
+    result: set[InterestingRegion] = set()
+    for co, data in DATABASE.items():
+        delta = _NAO - data["first_seen"]
+        if delta.days <= max_days:
+            d = InterestingRegion(data["first_seen"], data["current_name"], co)
+            result.add(d)
+    return result
+
 
 def main():
+    global DATABASE
+
     with DB_PATH.open("rb") as fin:
-        db: dict[CoordType, RegionsDBRecord3] = pickle.load(fin)
+        DATABASE = pickle.load(fin)
 
-    nao = datetime.now().astimezone()
-    interesting: list[tuple[datetime, str, CoordType]] = []
-    for co, data in db.items():
-        delta = nao - data["first_seen"]
-        if delta.days < CUTOFF:
-            d = data["first_seen"], data["current_name"], co
-            interesting.append(d)
-
-    interesting.sort()
+    interesting: list[InterestingRegion] = sorted(recent(CUTOFF))
     print(f"{len(interesting)} new regions the past {CUTOFF} days")
     new_areas: dict[str, list[CoordType]] = {}
     for i, (t, name, co) in enumerate(interesting, start=1):
         print(f"{i:>3}) {t.isoformat(timespec='minutes')} {name} {co}")
-        if name.startswith("RFL"):
-            new_areas.setdefault("RFL", []).append(co)
-        elif name.startswith("SLB"):
-            new_areas.setdefault("SLB", []).append(co)
-
-    print()
-    print(f"RFL => {AreaBounds.from_coordset(new_areas['RFL'])!r}")
-    print(f"SLB => {AreaBounds.from_coordset(new_areas['SLB'])!r}")
 
 
 if __name__ == '__main__':
