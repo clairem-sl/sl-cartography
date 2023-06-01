43f0efa54be79370e3c458d8f7955ae3db0a6321
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-06-01 13:07:50 +0700

    Major Changes
    Extract the fetcher logic (to be used by other fetchers)
    
    Simplify bonnie fetcher interface - no raw return

--- src/sl_maptools/fetchers/__init__.py
+++ src/sl_maptools/fetchers/__init__.py
@@ -1,15 +1,19 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import abc
+import asyncio
+import random
 from typing import NamedTuple
 
+import httpx
+
 from sl_maptools import MapCoord
+from sl_maptools.utils import QuietablePrint
 
 
 class FetcherConnectionError(ConnectionError):
-    def __init__(
-        self, *args, internal_errors: list[Exception] = None, coord: MapCoord = None
-    ):
+    def __init__(self, *args, internal_errors: list[Exception] = None, coord: MapCoord = None):
         super(FetcherConnectionError, self).__init__(*args)
         self.internal_errors = internal_errors or []
         self.coord = coord
@@ -28,3 +32,57 @@ class CookedResult(NamedTuple):
     coord: MapCoord
     result: str | None
     status_code: int = 0
+
+
+class Fetcher(metaclass=abc.ABCMeta):
+    URL_TEMPLATE: str = ""
+    RETRYABLE_EX: tuple[Exception, ...] = (httpx.ConnectTimeout, httpx.ReadTimeout, httpx.ReadError)
+
+    def __init__(self, a_session: httpx.AsyncClient):
+        """
+        Creates a Map Tile Getter with logic to retrieve map tiles
+
+        :param a_session: An Async client session
+        """
+        self.a_session: httpx.AsyncClient = a_session
+
+    async def async_get_raw(
+        self,
+        coord: MapCoord,
+        quiet: bool = False,
+        retries: int = 6,
+        raise_err: bool = True,
+    ) -> RawResult:
+        """ """
+        qprint = QuietablePrint(quiet, flush=True)
+        qprint(".", end="")
+        url = self.URL_TEMPLATE.format(x=coord.x, y=coord.y)
+        internal_errors = []
+        multiplier = 0.25
+        for _ in range(0, retries):
+            multiplier *= 2.0
+            await asyncio.sleep(random.random() * multiplier)
+            try:
+                response = await self.a_session.get(url)
+            except self.RETRYABLE_EX as e1:
+                # Not quietable
+                print(">", end="", flush=True)
+                internal_errors.append(e1)
+                continue
+            except Exception as e:
+                raise FetcherConnectionError(internal_errors=[e], coord=coord) from e
+
+            status_code = response.status_code
+
+            if status_code == 200:
+                qprint("+", end="")
+                return RawResult(coord, response.content, status_code)
+
+            # Don't quiet this
+            print(f"{status_code}?", end="", flush=True)
+            internal_errors.append(f"Unexpected HTTP status code {response.status_code}")
+            await asyncio.sleep(0.5)
+
+        print(f"ERR({coord})", end="", flush=True)
+        if raise_err:
+            raise FetcherConnectionError(internal_errors=internal_errors, coord=coord)
--- src/sl_maptools/fetchers/bonnie.py
+++ src/sl_maptools/fetchers/bonnie.py
@@ -4,14 +4,13 @@
 from __future__ import annotations
 
 import asyncio
-import random
-from typing import Any, Dict, NamedTuple, Optional, Protocol, Set
+import json
+from typing import Any, Dict, NamedTuple, Protocol, Set
 
 import httpx
 
 from sl_maptools import MapCoord
-from sl_maptools.fetchers import FetcherConnectionError
-from sl_maptools.utils import QuietablePrint
+from sl_maptools.fetchers import Fetcher
 
 
 class RawTile(NamedTuple):
@@ -30,89 +29,23 @@ class MapProgressProtocol(Protocol):
     last_fail_rows: Set[int] = set()
 
 
-_RETRYABLE_EX = (httpx.ConnectTimeout, httpx.ReadTimeout, httpx.ReadError)
-
-
-class NameFetcher(object):
+class BonnieFetcher(Fetcher):
     URL_TEMPLATE = "https://www.bonniebots.com/static-api/regions/{x}/{y}/index.json"
 
-    def __init__(self, a_session: httpx.AsyncClient):
-        """
-        Creates a Map Tile Getter with logic to retrieve map tiles
-
-        :param a_session: An Async client session
-        """
-        self.a_session: httpx.AsyncClient = a_session
-
-    async def async_get_name_raw(
+    async def async_get_data(
         self,
         coord: MapCoord,
         quiet: bool = False,
         retries: int = 2,
         raise_err: bool = True,
-    ) -> RawTile:
+    ) -> dict:
         """ """
-        qprint = QuietablePrint(quiet)
-        qprint(".", end="", flush=True)
-        url = self.URL_TEMPLATE.format(x=coord.x, y=coord.y)
-        internal_errors = []
-        multiplier = 0.25
-        for _ in range(0, retries):
-            multiplier *= 2.0
-            await asyncio.sleep(random.random() * multiplier)
-
-            for _ in range(0, 8):
-                mul2 = 0.5
-                try:
-                    response = await self.a_session.get(url)
-                    break
-                except _RETRYABLE_EX as e1:
-                    # Not quietable
-                    print(">", end="", flush=True)
-                    internal_errors.append(e1)
-                    await asyncio.sleep(random.random() * mul2)
-                    mul2 *= 2.0
-                    continue
-                except Exception as e:
-                    raise FetcherConnectionError(internal_errors=[e], coord=coord) from e
-            else:
-                break
-
-            status_code = response.status_code
-
-            if status_code == 200:
-                qprint("+", end="", flush=True)
-                data: dict[str, Any] = response.json()
-                if data.get("message") == "404 - Not Found":
-                    return RawTile(coord, None)
-                return RawTile(coord, data["region_name"].encode("utf-8"))
-
-            # Don't quiet this
-            print(f"{status_code}?", end="", flush=True)
-            internal_errors.append(
-                f"Unexpected HTTP status code {response.status_code}"
-            )
-            await asyncio.sleep(0.5)
-        print(f"ERR({coord})", end="", flush=True)
-        if raise_err:
-            raise FetcherConnectionError(internal_errors=internal_errors, coord=coord)
-
-    async def async_get_name(
-        self,
-        coord: MapCoord,
-        quiet: bool = False,
-        retries: int = 2,
-        raise_err: bool = True,
-    ) -> CookedTile:
-        _, rsltb = await self.async_get_name_raw(coord, quiet, retries, raise_err)
-        if rsltb is None:
-            return CookedTile(coord, None)
-        assert isinstance(rsltb, bytes)
-        rslt: str = rsltb.decode("utf-8")
-        return CookedTile(coord, rslt)
+        blob: bytes
+        _, blob, _ = await self.async_get_raw(coord, quiet, retries, raise_err)
+        return json.loads(blob.decode("utf-8"))
 
 
-class BoundedNameFetcher(NameFetcher):
+class BoundedBonnieFetcher(BonnieFetcher):
     """
     Wraps MapFetcher in a way to limit in-flight fetches.
 
@@ -128,7 +61,6 @@ class BoundedNameFetcher(NameFetcher):
         sema_size: int,
         async_session: httpx.AsyncClient,
         retries: int = 3,
-        cooked: bool = False,
     ):
         """
 
@@ -139,26 +71,15 @@ class BoundedNameFetcher(NameFetcher):
         super().__init__(a_session=async_session)
         self.sema = asyncio.Semaphore(sema_size)
         self.retries = retries
-        self.cooked = cooked
 
-    async def async_fetch(self, coord: MapCoord) -> Optional[RawTile | CookedTile]:
+    async def async_fetch(self, coord: MapCoord) -> dict:
         """Perform async fetch, but won't actually start fetching if semaphore is depleted."""
-        async with self.sema:
-            try:
-                if self.cooked:
-                    return await asyncio.wait_for(
-                        self.async_get_name(coord, quiet=True, retries=self.retries), 10
-                    )
-                else:
-                    return await asyncio.wait_for(
-                        self.async_get_name_raw(
-                            coord, quiet=True, retries=self.retries
-                        ),
-                        10,
-                    )
-            except asyncio.CancelledError:
-                print(f"{coord} cancelled")
-                return None
-            except (asyncio.TimeoutError, httpx.PoolTimeout):
-                print(f"{coord} Timeout!")
-                return None
+        try:
+            async with self.sema:
+                return await asyncio.wait_for(self.async_get_data(coord, quiet=True, retries=self.retries), 10)
+        except asyncio.CancelledError:
+            print(f"{coord} cancelled")
+            raise
+        except (asyncio.TimeoutError, httpx.PoolTimeout):
+            print(f"{coord} Timeout!")
+            raise
