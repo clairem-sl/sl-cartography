5db1cf84d588cadc42c3e971c93d043f10af6ef3
Claire Morgenthau <claire_morgenthau@outlook.com>
2023-06-05 01:10:07 +0700

    Exporter for DomC database to YAML

---
+++ src/worldmap_v4/export_domc_db.py
@@ -0,0 +1,40 @@
+import pickle
+from pathlib import Path
+from typing import Final
+
+from ruamel.yaml import YAML, RoundTripRepresenter
+
+from sl_maptools import CoordType
+from sl_maptools.utils import ConfigReader
+
+
+Config = ConfigReader("config.toml")
+
+DOMC_DB_PATH: Final[Path] = Path(Config.mosaic.dir) / Config.mosaic.domc_db
+
+
+def main():
+    print("Reading DB")
+    with DOMC_DB_PATH.open("rb") as fin:
+        domc_db: dict[CoordType, dict[Path, dict[int, list]]] = pickle.load(fin)
+
+    print("Transform DB")
+    out_db: dict[str, dict[str, dict[int, list]]] = {}
+    for co, data in sorted(domc_db.items(), key=lambda i: (i[0][1], i[0][0])):
+        inner = {}
+        for fp, domc in sorted(data.items()):
+            inner[str(fp)] = domc
+        x, y = co
+        out_db[f"{x},{y}"] = inner
+
+    print("Writing YAML")
+    yaml = YAML(typ="safe")
+    yaml.Representer = RoundTripRepresenter
+    with DOMC_DB_PATH.with_suffix(".yaml").open("wt") as fout:
+        yaml.dump(out_db, fout)
+
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
