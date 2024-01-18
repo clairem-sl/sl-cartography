0fe3c9a72ac47cfe126495d38ff647a81b624767
Claire Morgenthau <claire_morgenthau@outlook.com>
2024-01-18 22:22:05 +0700

    Optimize image_processing.py

--- src/sl_maptools/image_processing.py
+++ src/sl_maptools/image_processing.py
@@ -13,71 +13,35 @@ from skimage.metrics import structural_similarity as ssim
 BoxTuple = tuple[int, int, int, int]
 RGBTuple = tuple[int, int, int]
 
+
+def _gen_fascia(shift: int, size: int) -> list[BoxTuple]:
+    return [
+        (x, y, x + size, y + size)
+        for x in range(0, 257 - shift, shift)
+        for y in range(0, 257 - shift, shift)
+    ]
+
+
 FASCIA_COORDS: dict[int, list[BoxTuple]] = {
     1: [(0, 0, 256, 256)],
-    2: [(0, 0, 128, 128), (0, 128, 128, 256), (128, 0, 256, 128), (128, 128, 256, 256)],
-    3: [
-        (0, 0, 96, 96),
-        (0, 80, 96, 176),
-        (0, 160, 96, 256),
-        (80, 0, 176, 96),
-        (80, 80, 176, 176),
-        (80, 160, 176, 256),
-        (160, 0, 256, 96),
-        (160, 80, 256, 176),
-        (160, 160, 256, 256),
-    ],
-    4: [
-        (0, 0, 64, 64),
-        (0, 64, 64, 128),
-        (0, 128, 64, 192),
-        (0, 192, 64, 256),
-        (64, 0, 128, 64),
-        (64, 64, 128, 128),
-        (64, 128, 128, 192),
-        (64, 192, 128, 256),
-        (128, 0, 192, 64),
-        (128, 64, 192, 128),
-        (128, 128, 192, 192),
-        (128, 192, 192, 256),
-        (192, 0, 256, 64),
-        (192, 64, 256, 128),
-        (192, 128, 256, 192),
-        (192, 192, 256, 256),
-    ],
-    5: [
-        (0, 0, 56, 56),
-        (0, 50, 56, 106),
-        (0, 100, 56, 156),
-        (0, 150, 56, 206),
-        (0, 200, 56, 256),
-        (50, 0, 106, 56),
-        (50, 50, 106, 106),
-        (50, 100, 106, 156),
-        (50, 150, 106, 206),
-        (50, 200, 106, 256),
-        (100, 0, 156, 56),
-        (100, 50, 156, 106),
-        (100, 100, 156, 156),
-        (100, 150, 156, 206),
-        (100, 200, 156, 256),
-        (150, 0, 206, 56),
-        (150, 50, 206, 106),
-        (150, 100, 206, 156),
-        (150, 150, 206, 206),
-        (150, 200, 206, 256),
-        (200, 0, 256, 56),
-        (200, 50, 256, 106),
-        (200, 100, 256, 156),
-        (200, 150, 256, 206),
-        (200, 200, 256, 256),
-    ],
+    2: _gen_fascia(128, 128),
+    3: _gen_fascia(80, 96),
+    4: _gen_fascia(64, 64),
+    5: _gen_fascia(50, 56),
 }
 FASCIA_SIZES = sorted(FASCIA_COORDS.keys())
 
 
+DEFA_KMEAN: int = 3
+DEFA_KMEAN_DICT: dict[int, int] = {
+    1: 5,
+    2: 4,
+    0: DEFA_KMEAN,  # Represents all others
+}
+
+
 def calculate_dominant_colors(
-    region: Image.Image, fascia_per_side: int, kmeans: int = 3
+    region: Image.Image, fascia_per_side: int, kmeans: int | dict[int, int] | None = None
 ) -> list[RGBTuple]:
     """
     Given a region's image, calculate the dominant color per-fascia
@@ -91,6 +55,10 @@ def calculate_dominant_colors(
         raise KeyError(
             f"Valid fascia_per_side values: {', '.join(map(str, FASCIA_SIZES))}"
         )
+    if kmeans is None:
+        kmeans = DEFA_KMEAN_DICT
+    if isinstance(kmeans, dict):
+        kmeans = kmeans.get(fascia_per_side, kmeans.get(0, DEFA_KMEAN))
     dom_colors: list[RGBTuple] = []
     for fcoord in FASCIA_COORDS[fascia_per_side]:
         fascia = region.crop(fcoord)
@@ -120,7 +88,7 @@ DEFA_SIMILAR_THRESHOLDS: Final[SimilarityThresholds] = {
 }
 
 
-@dataclass
+@dataclass(slots=True, init=False, eq=False)
 class SimilarityResult:
     """Records the result of similarity test, with a string that contains the justification"""
 
