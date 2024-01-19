c49b2540712f12d52a587473fe257e360a26b268
Claire Morgenthau <claire_morgenthau@outlook.com>
2024-01-20 00:12:17 +0700

    New Tool: Retriever4.Maps.Area

---
+++ src/retriever_v4/maps/area.py
@@ -0,0 +1,99 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import argparse
+import asyncio
+import re
+from datetime import datetime
+from fnmatch import fnmatch
+from itertools import product
+from pathlib import Path
+from typing import TYPE_CHECKING, NamedTuple, cast
+
+import httpx
+
+from sl_maptools import RE_SLGI_NOTATION, AreaDescriptor, MapCoord
+from sl_maptools.fetchers.map import MapFetcher
+from sl_maptools.knowns import KNOWN_AREAS
+
+if TYPE_CHECKING:
+    from sl_maptools.fetchers import RawResult
+
+
+RE_BOXCOORDS = re.compile(r"(?P<x1>\d+),(?P<y1>\d+)[,-](?P<x2>\d+),(?P<y2>\d+)")
+
+
+class Options(NamedTuple):
+    """Represents options extracted from the CLI"""
+
+    out_dir: Path
+    area: list[str]
+
+
+def get_options() -> Options:
+    """Get option from the CLI"""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--out-dir", type=Path, default=Path())
+    parser.add_argument("area", type=str, nargs="+")
+
+    _opts = parser.parse_args()
+
+    return cast(Options, _opts)
+
+
+async def aretrieve(opts: Options, wants: dict[str, list[tuple[int, int, int, int]]]) -> None:
+    """Performs asynchronous retrieval of wanted areas"""
+    seen: set[tuple[int, int]] = set()
+    async with httpx.AsyncClient(http2=True) as aclient:
+        fetcher = MapFetcher(a_session=aclient)
+        for name, lista in wants.items():
+            print(f"Retrieving {name}:")
+            for x1, y1, x2, y2 in lista:
+                tasks = [
+                    asyncio.create_task(fetcher.async_get_region_raw(MapCoord(*xy), quiet=True))
+                    for xy in product(range(x1, x2 + 1), range(y1, y2 + 1))
+                    if xy not in seen
+                ]
+            for fut in asyncio.as_completed(tasks):
+                mr: RawResult = await fut
+                if mr.result is None:
+                    continue
+                x, y = mr.coord
+                ts = datetime.strftime(datetime.now(), "%y%m%d-%H%M")
+                targ = opts.out_dir / f"{x}-{y}_{ts}.jpg"
+                with targ.open("wb") as fout:
+                    fout.write(mr.result)
+                print(f"  {targ}")
+                seen.add((x, y))
+
+
+def main(opts: Options) -> None:  # noqa: D103
+    known_folded: dict[str, AreaDescriptor] = {k.casefold(): desc for k, desc in KNOWN_AREAS.items()}
+
+    wants: dict[str, list[tuple[int, int, int, int]]] = {}
+
+    for want in opts.area:
+        print(f"Want: {want}")
+        if m := RE_BOXCOORDS.match(want):
+            x1, y1, x2, y2 = m.groups()
+            wants[want] = [x1, y1, x2, y2]
+            continue
+        if m := RE_SLGI_NOTATION.match(want):
+            x1, y1, x2, y2 = m.groups()
+            wants[want] = [x1, y1, x2, y2]
+            continue
+        if (area_desc := known_folded.get(want.casefold())) is not None:
+            wants[want] = [a for a in area_desc.includes]
+            continue
+        for k, area_desc in known_folded.items():
+            if fnmatch(k, want.casefold()):
+                wants[want] = [a for a in area_desc.includes]
+    print(f"Parsed {len(wants)} areas")
+
+    if wants:
+        asyncio.run(aretrieve(opts, wants))
+
+
+if __name__ == "__main__":
+    main(get_options())
